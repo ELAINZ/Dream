6673dc3d50716c11e5383796143f6825bb92d78a
ELainZ <36255043+ELAINZ@users.noreply.github.com>
2026-01-19 15:26:57 -0600

    error

--- examples/run_sft_tulu3.sh
+++ examples/run_sft_tulu3.sh
@@ -22,7 +22,6 @@ torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
     data.truncation=right \
     optim.lr=2e-6 \
     data.micro_batch_size_per_gpu=8 \
-    data.perbatch_cutoff=True \
     data.perbatch_cutoff_type=random_with_input_pad \
     +data.perbatch_cutoff=True \
     model.hugging_face=Dream-org/Dream-v0-Base-7B \
